ee3749da018a95e9489c28c56e9055d03421a04a
quexs <quexs@qq.com>
2024-05-19 02:26:24 +0800

    新增轮子库：Java压缩、解压缩使用封装

--- CompatLib/src/main/java/com/quexs/compatlib/wheel/ZipJava.java
+++ CompatLib/src/main/java/com/quexs/compatlib/wheel/ZipJava.java
@@ -11,7 +11,10 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.zip.ZipEntry;
@@ -22,14 +25,14 @@ import java.util.zip.ZipOutputStream;
  * java 压缩库封装
  */
 public class ZipJava {
-    private final ThreadPoolExecutor newFixedThreadPool;
+    private final ThreadPoolExecutor cachedThreadPool;
     public ZipJava(){
         //设置最大并发数为3条
-        newFixedThreadPool = new ThreadPoolExecutor(3, 3,
-                50L, TimeUnit.MILLISECONDS,
-                new LinkedBlockingQueue<Runnable>());
+        cachedThreadPool = new ThreadPoolExecutor(0, Integer.MAX_VALUE,
+                60L, TimeUnit.MILLISECONDS,
+                new SynchronousQueue<Runnable>());
         //设置允许超时关闭
-        newFixedThreadPool.allowCoreThreadTimeOut(true);
+        cachedThreadPool.allowCoreThreadTimeOut(true);
     }
 
     /**
@@ -38,25 +41,25 @@ public class ZipJava {
      * @param targetFolderPath 压缩文件存放目录路径
      * @param zipListener 压缩监听类
      */
-    public void zip(File sourceFile,  String targetFolderPath, ZipListener zipListener){
-        if(newFixedThreadPool == null) return;
-        newFixedThreadPool.execute(new ZipRunnable(sourceFile, targetFolderPath, zipListener));
+    public void zip(File sourceFile, String targetFolderPath, ZipListener zipListener){
+        if(cachedThreadPool == null) return;
+        cachedThreadPool.execute(new ZipRunnable(sourceFile,targetFolderPath,zipListener));
     }
 
     /**
      * zip解压缩
-     * @param inZipFile
-     * @param targetPath
+     * @param sourceFile 源文件
+     * @param targetFolderPath 解压缩文件存放目录
      * @param unzipListener
      */
-    public void unzip(File inZipFile, String targetPath, UnzipListener unzipListener) {
-        if(newFixedThreadPool == null) return;
-        newFixedThreadPool.execute(new UnzipRunnable(inZipFile,targetPath,unzipListener));
+    public void unzip(File sourceFile, String targetFolderPath, UnzipListener unzipListener) {
+        if(cachedThreadPool == null) return;
+        cachedThreadPool.execute(new UnzipRunnable(sourceFile,targetFolderPath,unzipListener));
     }
 
     public void release(){
-        if(newFixedThreadPool == null) return;
-        newFixedThreadPool.shutdown();
+        if(cachedThreadPool == null) return;
+        cachedThreadPool.shutdown();
     }
 
     private class ZipRunnable implements Runnable{
@@ -64,24 +67,20 @@ public class ZipJava {
         private final String targetFolderPath;
         private final ZipListener zipListener;
 
-        byte[] buffer;
-        long compressedSize;
-        long sourceFileTotal;
         public ZipRunnable(File sourceFile, String targetFolderPath, ZipListener zipListener){
             this.sourceFile = sourceFile;
             this.targetFolderPath = targetFolderPath;
             this.zipListener = zipListener;
         }
 
+        byte[] buffer;
+        long compressedSize;
+        long sourceFileTotal;
+
         @Override
         public void run() {
             if(zipListener != null){
-                zipListener.onZipStart(sourceFile, targetFolderPath);
-            }
-            if(!sourceFile.exists()){
-                if(zipListener != null){
-                    zipListener.onZipEnd(false,null,  new IOException("file does not exist"));
-                }
+                zipListener.zipStart(sourceFile, targetFolderPath);
             }
             File targetFolder = new File(targetFolderPath);
             if(!targetFolder.exists()){
@@ -92,27 +91,18 @@ public class ZipJava {
             //计算源文件大小
             sourceFileTotal = calculateFileSize(sourceFile);
             buffer = new byte[1024];
-            ZipOutputStream zos = null;
-            try {
-                zos = new ZipOutputStream(new FileOutputStream(targetFile));
+            try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(targetFile))) {
                 toZipFile(sourceFile, "", zos);
-                if(zipListener != null){
-                    zipListener.onZipEnd(true, targetFile, null);
+                if (zipListener != null) {
+                    zipListener.zipEnd(targetFile, null);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
-                if(zipListener != null){
-                    zipListener.onZipEnd(false, null, e);
-                }
-            }finally {
-                if(zos != null){
-                    try {
-                        zos.closeEntry();
-                    } catch (IOException e) {
-                        // 处理关闭时可能出现的异常
-                    }
+                if (zipListener != null) {
+                    zipListener.zipEnd(null, e);
                 }
             }
+
         }
 
         private void toZipFile(File sourceFile, String folderPath, ZipOutputStream zos) throws IOException {
@@ -122,32 +112,21 @@ public class ZipJava {
                     toZipFile(childFile,folderPath + File.separator + sourceFile.getName(), zos);
                 }
             }else {
-                BufferedInputStream bis = null;
-                try {
-                    bis = new BufferedInputStream(new FileInputStream(sourceFile));
+                try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(sourceFile))) {
                     zos.putNextEntry(new ZipEntry(folderPath + File.separator + sourceFile.getName()));
                     int len;
-                    while ((len = bis.read(buffer)) != -1){
+                    while ((len = bis.read(buffer)) != -1) {
                         zos.write(buffer, 0, len);
                         //累计已压缩的文件大小
                         compressedSize += len;
-                        if(zipListener != null){
-                            zipListener.onZipProgress((int)(compressedSize * 1d / sourceFileTotal * 100), compressedSize, sourceFileTotal);
+                        if (zipListener != null) {
+                            zipListener.zipProgress((int) (compressedSize * 1d / sourceFileTotal * 100), compressedSize, sourceFileTotal);
                         }
                     }
-                }finally {
-                    if(bis != null){
-                        try {
-                            bis.close();
-                        } catch (IOException e) {
-                            // 处理关闭时可能出现的异常
-                        }
-                    }
+                    zos.closeEntry();
                 }
-
             }
         }
-
     }
 
     /**
@@ -171,7 +150,6 @@ public class ZipJava {
     }
 
 
-
     /**
      * Zip解压缩
      */
@@ -188,79 +166,52 @@ public class ZipJava {
         @Override
         public void run() {
             if(unzipListener != null){
-                unzipListener.onUnzipStart(sourceFile, targetFolderPath);
-            }
-            if(!sourceFile.exists()){
-                if(unzipListener != null){
-                    unzipListener.onUnzipEnd(false, null, new IOException("file does not exist"));
-                }
+                unzipListener.unzipStart(sourceFile, targetFolderPath);
             }
             File targetFolder = new File(targetFolderPath,sourceFile.getName().substring(0, sourceFile.getName().lastIndexOf(".")));
             if(targetFolder.exists()){
                 FileUtils.deleteFile(targetFolder);
             }
             targetFolder.mkdirs();
-            ZipFile zipFile = null;
-            try {
-                zipFile = new ZipFile(sourceFile);
+            try (ZipFile zipFile = new ZipFile(sourceFile)) {
                 long unzipTotal = 0;
-                if(unzipListener != null){
+                if (unzipListener != null) {
                     unzipTotal = unzipTotal(zipFile);
                 }
                 byte[] buffer = new byte[1024];
                 int len;
                 long unzipSize = 0;
                 Enumeration<? extends ZipEntry> entries = zipFile.entries();
-                while (entries.hasMoreElements()){
+                while (entries.hasMoreElements()) {
                     ZipEntry zipEntry = entries.nextElement();
                     //处理乱码问题
                     boolean canEnCode = Charset.forName("GBK").newEncoder().canEncode(zipEntry.getName());
                     String fileName = canEnCode ? zipEntry.getName() : new String(zipEntry.getName().getBytes(StandardCharsets.UTF_8), Charset.forName("GBK"));
-                    if("../".equalsIgnoreCase(fileName)){
+                    if ("../".equalsIgnoreCase(fileName)) {
                         continue;
                     }
                     File entryDest = new File(targetFolder, fileName);
-                    if(zipEntry.isDirectory()){
+                    if (zipEntry.isDirectory()) {
                         entryDest.mkdirs();
                     }
-                    BufferedInputStream bis = null;
-                    BufferedOutputStream bos = null;
-                    try {
-                        bis = new BufferedInputStream(zipFile.getInputStream(zipEntry));
-                        bos = new BufferedOutputStream(new FileOutputStream(entryDest));
-                        while ((len = bis.read(buffer)) != -1){
-                            bos.write(buffer,0,len);
+                    try (BufferedInputStream bis = new BufferedInputStream(zipFile.getInputStream(zipEntry)); BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(entryDest))) {
+                        while ((len = bis.read(buffer)) != -1) {
+                            bos.write(buffer, 0, len);
                             unzipSize += len;
-                            if(unzipListener != null){
-                                unzipListener.onUnzipProgress((int)(unzipSize * 1d / unzipTotal * 100),unzipSize, unzipTotal);
+                            if (unzipListener != null) {
+                                unzipListener.unzipProgress((int) (unzipSize * 1d / unzipTotal * 100), unzipSize, unzipTotal);
                             }
                         }
                         bos.flush();
-                    }finally {
-                        try {
-                            bos.close();
-                            bis.close();
-                        }catch (IOException e){
-
-                        }
-
                     }
                 }
-                if(unzipListener != null){
-                    unzipListener.onUnzipEnd(true, targetFolder, null);
+                if (unzipListener != null) {
+                    unzipListener.unzipEnd(targetFolder, null);
                 }
-            }catch (IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
-                if(unzipListener != null){
-                    unzipListener.onUnzipEnd(false, null, e);
-                }
-            }finally {
-                if(zipFile != null){
-                    try {
-                        zipFile.close();
-                    } catch (IOException e) {
-
-                    }
+                if (unzipListener != null) {
+                    unzipListener.unzipEnd(null, e);
                 }
             }
         }
@@ -285,22 +236,22 @@ public class ZipJava {
     }
 
     public interface UnzipListener{
-        void onUnzipStart(File inZipFile, String targetFolderPath);
+        void unzipStart(File inZipFile, String targetFolderPath);
         /**
          *
          * @param unzipSize 解压缩文件大小
          * @param unzipTotal 解压缩后文件总大小
          */
-        void onUnzipProgress(int progress,long unzipSize, long unzipTotal);
+        void unzipProgress(int progress,long unzipSize, long unzipTotal);
 
-        void onUnzipEnd(boolean isSuccess,File unzipFile,IOException e);
+        void unzipEnd(File unzipFile,IOException e);
 
     }
 
     public interface ZipListener{
-        void onZipStart(File inZipFile, String targetFolderPath);
-        void onZipProgress(int progress, long compressedSize, long sourceFileTotal);
-        void onZipEnd(boolean isSuccess, File zipFile, IOException e);
+        void zipStart(File inZipFile, String targetFolderPath);
+        void zipProgress(int progress, long compressedSize, long sourceFileTotal);
+        void zipEnd(File zipFile, IOException e);
     }
 
 
